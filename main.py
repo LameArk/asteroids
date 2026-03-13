fd0fb07b8eff9fc830baf88b81af13abca1320d6
LameArk <JulianLamark@my.unt.edu>
2026-03-12 22:08:25 -0500

    Player Triangle sprite appears

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, LINE_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,6 +10,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_ship = Player(x,y)
+
     end_loop = False
 
     while (end_loop == False):
@@ -16,7 +24,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            game_clock.tick(60) 
+            dt = game_clock.tick(60) / 1000
         screen.fill("black")
+        player_ship.draw(screen, "white", player_ship.triangle(), LINE_WIDTH)
         pygame.display.flip()
 
 
